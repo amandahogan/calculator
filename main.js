ac6ba23e14ad1b9d72234ff287c8742e4cd287d4
amandahogan <hogan.ans@gmail.com>
2016-11-23 22:31:45 +1300

    add pseudocode

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
   document.getElementsByClass("number").addEventListener('click', displayNumber);
 
   function displayNumber(){
-      
+
   },
 
 
 });
+//seperate buttons between numbers and operations and clear
+//run function to display button value onclick for numbers
+//run function to display button value onclick for numbers
+//run function to clearall if button AC is pressed
+//run function to remove last value if clear button is pressed
+//function for = sign to displayCalc by taking the value in displayEquation return as numbers
